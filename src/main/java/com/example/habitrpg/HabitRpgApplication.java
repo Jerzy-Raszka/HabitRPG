3454571e0bbeedaefcf16c1bb6ff9f9b97552214
Jerzy Raszka <jerzyraszka2000@gmail.com>
2025-10-02 15:16:22 +0200

    feat: initial commit

---
+++ src/main/java/com/example/habitrpg/HabitRpgApplication.java
@@ -0,0 +1,13 @@
+package com.example.habitrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitRpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitRpgApplication.class, args);
+    }
+
+}
