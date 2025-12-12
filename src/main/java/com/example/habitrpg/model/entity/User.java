0aec424b21bad9bbf574d89aaf565e8b7b2cf2fc
Jerzy Raszka <jerzyraszka2000@gmail.com>
2025-12-12 08:00:33 +0100

    refactor: made requirement for username to be unique

--- src/main/java/com/example/habitrpg/model/entity/User.java
+++ src/main/java/com/example/habitrpg/model/entity/User.java
@@ -10,6 +10,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
+    @Column(unique = true)
     private String username;
     private String password;
     private Integer level;
